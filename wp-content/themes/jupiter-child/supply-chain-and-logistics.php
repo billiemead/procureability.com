e5db69beaa83e468d183011e920801e2c820c648
brnteka <mpbrainwave@gmail.com>
2022-09-13 18:42:40 +0300

    [*] supply chain and logistics page edits

--- wp-content/themes/jupiter-child/supply-chain-and-logistics.php
+++ wp-content/themes/jupiter-child/supply-chain-and-logistics.php
@@ -7,67 +7,83 @@
 
 <?php get_header(); ?>
 
-    <!--Hero-->
 <?php get_template_part('partials/page', 'hero'); ?>
-    <!--Hero-->
 
-    <!--Content-->
 <?php if (have_posts()) : ?>
+    <!--Content-->
     <?php while (have_posts()) : the_post(); ?>
         <?php get_template_part('partials/page', 'content'); ?>
     <?php endwhile; ?>
-<?php endif; ?>
     <!--Content-->
+<?php endif; ?>
 
-    
 
-    <!--Examples-->
-<?php $examples_background_image = get_field('examples_background_image'); ?>
-    <section class="examples rocket-lazyload" data-bg="<?= $examples_background_image['url']; ?>">
-        <div class="examples-container">
-            <?php $examples_title = get_field('examples_title'); ?>
-            <?php if ($examples_title) : ?>
-                <div class="examples-title"><?= $examples_title; ?></div>
+<?php $experience_title = get_field('experience_title'); ?>
+<?php $experience_tiles = get_field('experience_tiles'); ?>
+<?php if ($experience_title || $experience_tiles) : ?>
+    <!--Experience-->
+    <section class="section section--experience">
+        <div class="section-container">
+            <?php if ($experience_title) : ?>
+                <div class="section-title">
+                    <?= $experience_title; ?>
+                </div>
             <?php endif; ?>
-            <?php $examples_list = get_field('examples_list'); ?>
-            <?php if ($examples_list) : ?>
-                <ul class="examples-list">
-                    <?php foreach ($examples_list as $examples_list_item) : ?>
-                        <li class="examples-list__item">
-                            <svg class="examples-list__item-icon" xmlns="http://www.w3.org/2000/svg"
-                                 viewBox="0 0 1792 1792">
-                                <path d="M1671 566q0 40-28 68l-724 724-136 136q-28 28-68 28t-68-28l-136-136-362-362q-28-28-28-68t28-68l136-136q28-28 68-28t68 28l294 295 656-657q28-28 68-28t68 28l136 136q28 28 28 68z"></path>
-                            </svg>
-                            <span><?= $examples_list_item['example']; ?></span></li>
+
+            <?php if ($experience_tiles) : ?>
+                <div class="experience-tiles">
+                    <?php foreach ($experience_tiles as $experience_tile) : ?>
+                        <div class="experience-tile">
+                            <div class="experience-tile__icon">
+                                <?= file_get_contents($experience_tile['icon']['url']) ?>
+                            </div>
+                            <div class="experience-tile__title"><?= $experience_tile['title']; ?></div>
+                            <div class="experience-tile__text"><?= $experience_tile['text']; ?></div>
+                        </div>
                     <?php endforeach; ?>
-                </ul>
+                </div>
             <?php endif; ?>
         </div>
     </section>
-    <!--Examples-->
+    <!--Experience-->
+<?php endif; ?>
 
+    <!--Form section-->
+<?php get_template_part('partials/page', 'form-section'); ?>
+    <!--Form section-->
+
+
+<?php $program_outline_title = get_field('program_outline_title'); ?>
+<?php $program_outline_image = get_field('program_outline_image'); ?>
+<?php if ($program_outline_title || $program_outline_image) : ?>
     <!--Program outline-->
     <section class="section section--program-outline">
         <div class="section-container">
-			<?php $program_outline_title = get_field('program_outline_title'); ?>
             <?php if ($program_outline_title) : ?>
                 <div class="section-title section-title--center"><?= $program_outline_title; ?></div>
             <?php endif; ?>
-			<img class="imagezed" src="https://procureability.com/wp-content/uploads/2022/09/2_graph.png">
-           
+            <?php if ($program_outline_image) : ?>
+                <img class="program-outline-image" src="<?= $program_outline_image['url']; ?>"
+                     alt="<?= $program_outline_image['alt']; ?>">
+            <?php endif; ?>
         </div>
     </section>
     <!--Program outline-->
+<?php endif; ?>
 
+
+<?php $faq_title = get_field('faq_title'); ?>
+<?php $faq_list = get_field('faq_list'); ?>
+<?php if ($faq_title || $faq_list) : ?>
     <!--FAQ-->
     <section class="section section--faq">
         <div class="section-container section-container--slightly-wider">
-            <?php $faq_title = get_field('faq_title'); ?>
+
             <?php if ($faq_title) : ?>
                 <div class="section-title"><?= $faq_title; ?></div>
             <?php endif; ?>
 
-            <?php $faq_list = get_field('faq_list'); ?>
+
             <?php if ($faq_list) : ?>
                 <div class="faqs">
                     <?php foreach ($faq_list as $key => $faq_list_item) : ?>
@@ -87,10 +103,36 @@
         </div>
     </section>
     <!--FAQ-->
+<?php endif; ?>
 
-    <!--Form section-->
-<?php get_template_part('partials/page', 'form-section'); ?>
-    <!--Form section-->
+<?php $examples_title = get_field('examples_title'); ?>
+<?php $examples_list = get_field('examples_list'); ?>
+<?php if ($examples_title || $examples_list) : ?>
+    <!--Examples-->
+    <?php $examples_background_image = get_field('examples_background_image'); ?>
+    <section class="examples rocket-lazyload" data-bg="<?= $examples_background_image['url']; ?>">
+        <div class="examples-container">
+
+            <?php if ($examples_title) : ?>
+                <div class="examples-title"><?= $examples_title; ?></div>
+            <?php endif; ?>
+
+            <?php if ($examples_list) : ?>
+                <ul class="examples-list">
+                    <?php foreach ($examples_list as $examples_list_item) : ?>
+                        <li class="examples-list__item">
+                            <svg class="examples-list__item-icon" xmlns="http://www.w3.org/2000/svg"
+                                 viewBox="0 0 1792 1792">
+                                <path d="M1671 566q0 40-28 68l-724 724-136 136q-28 28-68 28t-68-28l-136-136-362-362q-28-28-28-68t28-68l136-136q28-28 68-28t68 28l294 295 656-657q28-28 68-28t68 28l136 136q28 28 28 68z"></path>
+                            </svg>
+                            <span><?= $examples_list_item['example']; ?></span></li>
+                    <?php endforeach; ?>
+                </ul>
+            <?php endif; ?>
+        </div>
+    </section>
+    <!--Examples-->
+<?php endif; ?>
 
     <!--Studies-->
     <section class="section">
