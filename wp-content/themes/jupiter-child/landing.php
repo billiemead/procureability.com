0d16575d502cce840268d9c1683338deafc4d452
brnteka <mpbrainwave@gmail.com>
2022-09-30 13:22:18 +0300

    [*] paragraphs spacing edits

--- wp-content/themes/jupiter-child/landing.php
+++ wp-content/themes/jupiter-child/landing.php
@@ -10,7 +10,7 @@
 <section class="pt-[44px] pb-[50px] lg:pt-[88px] lg:pb-[100px]">
     <div class="max-w-[1230px] px-[15px] mx-auto">
         <h1 class="font-poppins text-[32px] lg:text-[48px] leading-[1.2] font-[600] text-[#393A40] mb-[0.66em]"><?= get_the_title(); ?></h1>
-        <div class="[&_p]:text-[18px] lg:[&_p]:text-[28px] [&_p]:leading-[1.6] [&_p]:text-[#393A40] [&_p]:mb-[1.14em] [&_p]:last:mb-0">
+        <div class="[&_p]:text-[18px] lg:[&_p]:text-[28px] [&_p]:leading-[1.6] [&_p]:text-[#393A40] [&_p]:mb-[1.14em] [&_p:last-child]:mb-0">
             <?php the_content(); ?>
         </div>
     </div>
