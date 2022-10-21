e2907ad94ba95ee5413f194ea6de1aae2d0bf92b
brnteka <mpbrainwave@gmail.com>
2022-10-21 20:32:31 +0300

    [*] hero spacing edits

--- wp-content/themes/jupiter-child/partials/home-hero.php
+++ wp-content/themes/jupiter-child/partials/home-hero.php
@@ -13,7 +13,7 @@
 <section
         class="bg-no-repeat bg-center bg-cover lg:bg-[length:118%_auto] lg:bg-[position:right_calc(50%-85px)] xxl:bg-[position:right_top_55%]"
         style="background-image: url('<?= $frontpage_hero_background_image['url'] ?>')">
-    <div class="pt-[104px] md:pt-[133px] xl:pt-[300px] pb-[60px] md:pb-[74px] xl:pb-[150px] xxl:pt-[480px]">
+    <div class="pt-[104px] md:pt-[133px] xl:pt-[400px] pb-[60px] md:pb-[74px] xl:pb-[150px] xxl:pt-[480px]">
         <div class="px-[34px] xl:max-w-[1096px] mx-auto">
             <div class="text-center font-poppins text-white text-[28px] md:text-[42px] leading-[1.2] md:leading-[1.27] font-[600] mb-[16px]">
                 <?= $frontpage_hero_title; ?>
