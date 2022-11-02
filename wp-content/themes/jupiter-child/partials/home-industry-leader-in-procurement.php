1d606c4702623b42b67c86a6d0d36b3bec8e940a
brnteka <mpbrainwave@gmail.com>
2022-11-02 17:31:00 +0200

    [*] front page logos slider edits

--- wp-content/themes/jupiter-child/partials/home-industry-leader-in-procurement.php
+++ wp-content/themes/jupiter-child/partials/home-industry-leader-in-procurement.php
@@ -13,36 +13,34 @@
             <p><?= $frontpage_industry_leader_in_procurement_text; ?></p>
         </div>
         <?php if ($frontpage_industry_leader_in_procurement_clients) : ?>
-        <div class="md:max-w-[632px] xl:max-w-[1200px] mx-auto">
-            <div class="relative">
-                <div id="industryLeaders" class="swiper">
-                    <div class="swiper-wrapper">
-                        <?php foreach ($frontpage_industry_leader_in_procurement_clients as $client) : ?>
-                            <div class="swiper-slide !h-auto min-h-[64px] flex justify-center items-center [&_img]:w-auto [&_img]:max-h-[80px] [&_img]:h-auto [&_img]:max-w-full">
-                                <?= wp_get_attachment_image($client['image']['id'], 'full'); ?>
-                            </div>
-                        <?php endforeach; ?>
-                    </div>
+        <div class="relative">
+            <div id="industryLeaders" class="swiper md:max-w-[632px] xl:max-w-[1200px] mx-auto">
+                <div class="swiper-wrapper">
+                    <?php foreach ($frontpage_industry_leader_in_procurement_clients as $client) : ?>
+                        <div class="swiper-slide !h-auto min-h-[64px] flex flex-row justify-center items-end [&_img]:w-auto [&_img]:max-h-[80px] [&_img]:h-auto [&_img]:max-w-full">
+                            <?= wp_get_attachment_image($client['image']['id'], 'full'); ?>
+                        </div>
+                    <?php endforeach; ?>
                 </div>
-                <div id="industryLeadersNavigationPrev"
-                     class="absolute left-[-20px] md:left-[-30px] top-[50%] z-50 translate-y-[-50%] [&.swiper-button-disabled]:opacity-30 [&.swiper-button-disabled]:cursor-default [&:not(.swiper-button-disabled)]:hover:opacity-70 cursor-pointer text-[#0098AE] h-[20px] w-[20px] inline-flex justify-center items-center">
-                    <svg class="relative xl:left-[-3px]" width="13" height="23" viewBox="0 0 13 23" fill="none"
-                         xmlns="http://www.w3.org/2000/svg">
-                        <path d="M11.7676 1.82773L1.76758 11.8277L11.7676 21.8277" stroke="currentColor"
-                              stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                    </svg>
-                </div>
-                <div id="industryLeadersNavigationNext"
-                     class="absolute right-[-20px] md:right-[-30px] z-50 top-[50%] z-50 translate-y-[-50%] [&.swiper-button-disabled]:opacity-30 [&.swiper-button-disabled]:cursor-default [&:not(.swiper-button-disabled)]:hover:opacity-70 cursor-pointer text-[#0098AE] h-[20px] w-[20px] inline-flex justify-center items-center">
-                    <svg class="relative xl:right-[-3px]" width="13" height="23" viewBox="0 0 13 23" fill="none"
-                         xmlns="http://www.w3.org/2000/svg">
-                        <path d="M1.23242 21.6945L11.2324 11.6945L1.23243 1.69449" stroke="currentColor"
-                              stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                    </svg>
-                </div>
+                <div id="industryLeadersPagination"
+                     class="hidden md:flex md:flex-wrap mt-[32px] justify-center [&_span]:!mx-[8px] [&_span]:!w-[6px] [&_span]:!h-[6px] [&_span]:rounded-full [&_span]:!bg-[#DDE5E5] [&_span.swiper-pagination-bullet-active]:!bg-[#3BA17C] [&_span]:!opacity-100"></div>
             </div>
-            <div id="industryLeadersPagination"
-                 class="hidden md:flex md:flex-wrap mt-[32px] justify-center [&_span]:!mx-[8px] [&_span]:!w-[6px] [&_span]:!h-[6px] [&_span]:rounded-full [&_span]:!bg-[#DDE5E5] [&_span.swiper-pagination-bullet-active]:!bg-[#3BA17C] [&_span]:!opacity-100"></div>
+            <div id="industryLeadersNavigationPrev"
+                 class="md:hidden absolute xl:!static left-[-20px] top-[50%] z-50 translate-y-[-50%] [&.swiper-button-disabled]:opacity-30 [&.swiper-button-disabled]:cursor-default [&:not(.swiper-button-disabled)]:hover:opacity-70 cursor-pointer xl:bg-[#cdeaf0] text-[#0098AE] h-[20px] w-[20px] xl:w-[60px] xl:h-[60px] rounded-full inline-flex justify-center items-center xl:mr-[10px]">
+                <svg class="relative xl:left-[-3px]" width="13" height="23" viewBox="0 0 13 23" fill="none"
+                     xmlns="http://www.w3.org/2000/svg">
+                    <path d="M11.7676 1.82773L1.76758 11.8277L11.7676 21.8277" stroke="currentColor"
+                          stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                </svg>
+            </div>
+            <div id="industryLeadersNavigationNext"
+                 class="md:hidden absolute xl:!static right-[-20px] z-50 top-[50%] z-50 translate-y-[-50%] [&.swiper-button-disabled]:opacity-30 [&.swiper-button-disabled]:cursor-default [&:not(.swiper-button-disabled)]:hover:opacity-70 cursor-pointer xl:bg-[#cdeaf0] text-[#0098AE] h-[20px] w-[20px] xl:w-[60px] xl:h-[60px] rounded-full inline-flex justify-center items-center">
+                <svg class="relative xl:right-[-3px]" width="13" height="23" viewBox="0 0 13 23" fill="none"
+                     xmlns="http://www.w3.org/2000/svg">
+                    <path d="M1.23242 21.6945L11.2324 11.6945L1.23243 1.69449" stroke="currentColor"
+                          stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                </svg>
+            </div>
         </div>
         <?php endif; ?>
     </div>
