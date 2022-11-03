5ce104d101e90605bf276248d34ad717d16f2fdb
brnteka <mpbrainwave@gmail.com>
2022-11-03 18:13:19 +0200

    [+] digital-services video section

--- wp-content/themes/jupiter-child/digital-services.php
+++ wp-content/themes/jupiter-child/digital-services.php
@@ -200,6 +200,29 @@
         </section>
         <!--!analytics-->
 
+        <?php $video_title = get_field('video_title'); ?>
+        <?php $video_video = get_field('video_video'); ?>
+        <?php if ($video_title || $video_video) : ?>
+            <!--Video-->
+            <section id="video" class="pb-[30px]">
+                <div class="max-w-[930px] px-[15px] mx-auto">
+                    <div class="px-[15px] pt-[15px] pb-[40px]">
+                        <?php if ($video_video) : ?>
+                            <div>
+                                <?= $video_video; ?>
+                            </div>
+                        <?php endif; ?>
+                        <?php if ($video_title) : ?>
+                            <div class="text-center mt-[20px]">
+                                <div class="text-[19px] leading-black text-[#000] font-[700] capitalize"><?= $video_title; ?></div>
+                            </div>
+                        <?php endif; ?>
+                    </div>
+                </div>
+            </section>
+            <!--Video-->
+        <?php endif; ?>
+
         <!--benefits-->
         <section class="bg-[#F7F9FA] pt-[32px] pb-[39px] xl:pt-[123px] xl:pb-[117px]">
             <div class="px-[33px] xl:px-0 md:max-w-[464px] xl:max-w-[1084px] mx-auto">
