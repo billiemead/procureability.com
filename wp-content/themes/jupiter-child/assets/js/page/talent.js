34139c5694ddc3a120ee1023f46026ed5b2a9428
brnteka <mpbrainwave@gmail.com>
2022-11-18 12:30:50 +0200

    [+] talent page

--- wp-content/themes/jupiter-child/assets/js/page/talent.js
+++ wp-content/themes/jupiter-child/assets/js/page/talent.js
@@ -43,10 +43,94 @@ new Swiper('#reasonsToJoin', {
         //     slidesPerView: 2,
         //     spaceBetween: 34,
         // },
-        1240: {
+        1240: {}
+    }
+});
 
+new Swiper('#industryLeaders', {
+    loop: false,
+    spaceBetween: 18,
+    navigation: {
+        nextEl: '#industryLeadersNavigationNext',
+        prevEl: '#industryLeadersNavigationPrev',
+    },
+    pagination: {
+        el: '#industryLeadersPagination',
+        clickable: true,
+        dynamicBullets: true
+    },
+    breakpoints: {
+        768: {
+            slidesPerView: 4,
+            spaceBetween: 40,
+        },
+        1240: {
+            slidesPerView: 6,
+            spaceBetween: 60,
+        }
+    }
+});
+
+new Swiper('#clientSuccessStories', {
+    loop: false,
+    slidesPerView: 1,
+    spaceBetween: 34,
+    // navigation: {
+    //     nextEl: '.swiper-button-next',
+    //     prevEl: '.swiper-button-prev',
+    // },
+    pagination: {
+        el: '#clientSuccessStoriesPagination',
+        clickable: true,
+    },
+    breakpoints: {
+        768: {
+            slidesPerView: 2,
+        },
+        1240: {
+            slidesPerView: 3,
+            spaceBetween: 24,
         }
     }
 });
 
 
+const breakpoint = window.matchMedia('(min-width: 1240px)');
+
+let teamSwiper;
+
+const breakpointChecker = function () {
+    if (breakpoint.matches === true) {
+        if (teamSwiper !== undefined) {
+            teamSwiper.destroy(true, true);
+        }
+        return;
+    } else if (breakpoint.matches === false) {
+        enableTeamSwiper();
+    }
+};
+
+const enableTeamSwiper = function () {
+    teamSwiper = new Swiper('#ourProfessionalTeam', {
+        loop: true,
+        slidesPerView: 1,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        breakpoints: {
+            768: {
+                slidesPerView: 2,
+                spaceBetween: 20,
+            },
+        }
+    });
+};
+
+breakpoint.addEventListener("change", breakpointChecker);
+
+breakpointChecker();
