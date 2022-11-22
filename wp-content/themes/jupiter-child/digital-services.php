0bf5c00bc34449f9cacdcd94404cb496e3ed163b
brnteka <mpbrainwave@gmail.com>
2022-11-22 16:16:44 +0200

    [*] digital services page form position edits

--- wp-content/themes/jupiter-child/digital-services.php
+++ wp-content/themes/jupiter-child/digital-services.php
@@ -83,7 +83,7 @@
                             </ul>
                         <?php endif; ?>
                     </div>
-                    <div class="xl:w-[calc((555/1200)*100%)] mb-[-64px]">
+                    <div class="xl:w-[calc((555/1200)*100%)]">
                         <div id="form"
                              class="relative bg-white w-full md:rounded-[6px] shadow-[0px_3px_20px_rgba(57,58,64,0.1)] py-[32px] px-[35px] md:pt-[40px] md:pb-[48px] md:px-[78px] xl:px-[54px] xl:pt-[50px] xl:pb-[46px] box-border">
                             <?php $form_name = get_field('form_name'); ?>
