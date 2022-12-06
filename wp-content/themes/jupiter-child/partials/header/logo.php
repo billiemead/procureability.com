d9e66ac7e1e82b365707ae62aeb2e53e943762a1
brnteka <mpbrainwave@gmail.com>
2022-12-06 23:03:31 +0200

    [*] header edits

--- wp-content/themes/jupiter-child/partials/header/logo.php
+++ wp-content/themes/jupiter-child/partials/header/logo.php
@@ -1,5 +1,5 @@
 <a href="<?= get_home_url(); ?>">
-    <div :class="{'xl:block': !sticky || isTransparent }" class="hidden">
+    <div :class="{'xl:block': !sticky && isTransparent }" class="hidden">
         <?= wp_get_attachment_image(get_field('header_logo_light', 'option')['id'], 'full', false, array('class' => 'lazy-load-ignore block absolute top-[8px] w-[151px] h-auto')); ?>
     </div>
     <div :class="{'xl:!block' : sticky || !isTransparent}" class="xl:hidden">
