3a784f2e0f1be1926f1f00fc1a3b1dafdff6efcb
brnteka <mpbrainwave@gmail.com>
2022-12-15 18:49:02 +0200

    [+] solutions page

---
+++ wp-content/themes/jupiter-child/solutions.php
@@ -0,0 +1,22 @@
+<?php
+/*
+    Template Name: Solutions
+    Template Post Type: page
+*/
+?>
+
+<?php get_header(); ?>
+
+<?php /** HERO */ ?>
+<?php get_template_part('partials/solutions/hero'); ?>
+<?php /** !HERO */ ?>
+
+<?php /** TEAM */ ?>
+<?php get_template_part('partials/solutions/team'); ?>
+<?php /** !TEAM */ ?>
+
+<?php /** INSIGHTS */ ?>
+<?php get_template_part('partials/solutions/insights'); ?>
+<?php /** !INSIGHTS */ ?>
+
+<?php get_footer(); ?>
