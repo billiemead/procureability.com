853e8ef07520e7060aaecc71330aefa20f0120a1
brnteka <mpbrainwave@gmail.com>
2022-12-17 18:21:10 +0200

    [*] solutions page edits

--- wp-content/themes/jupiter-child/partials/solutions/features.php
+++ wp-content/themes/jupiter-child/partials/solutions/features.php
@@ -3,7 +3,7 @@
 <?php if ($features) : ?>
     <section class="py-[80px]" x-data="{activeIndex: 0, numberOfItems: <?= count($features); ?>}">
         <div class="max-w-[1200px] mx-auto px-[34px] lg:px-[60px] relative box-border">
-            <button class="hidden lg:flex justify-center items-center absolute top-[250px] left-[4px] w-[40px] h-[40px] rounded-full bg-transparent border border-[#393A40] border-solid text-[#393A40] hover:border-[rgba(97,99,107,0.3)] hover:text-[rgba(97,99,107,0.3)] transition-colors"
+            <button class="hidden lg:flex justify-center items-center absolute top-[235px] left-[4px] w-[40px] h-[40px] rounded-full bg-transparent border border-[#393A40] border-solid text-[#393A40] hover:border-[rgba(97,99,107,0.3)] hover:text-[rgba(97,99,107,0.3)] transition-colors"
                     @click="activeIndex = activeIndex === 0 ? numberOfItems - 1 : activeIndex - 1">
                 <svg width="9" height="16" viewBox="0 0 9 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M7.75037 1.35406L0.981135 8.1233L7.75037 14.8925" stroke="currentColor"
@@ -11,7 +11,7 @@
                           stroke-linecap="round" stroke-linejoin="round"/>
                 </svg>
             </button>
-            <button class="hidden lg:flex justify-center items-center absolute top-[250px] right-[4px] w-[40px] h-[40px] rounded-full bg-transparent border border-[#393A40] border-solid text-[#393A40] hover:border-[rgba(97,99,107,0.3)] hover:text-[rgba(97,99,107,0.3)] transition-colors"
+            <button class="hidden lg:flex justify-center items-center absolute top-[235px] right-[4px] w-[40px] h-[40px] rounded-full bg-transparent border border-[#393A40] border-solid text-[#393A40] hover:border-[rgba(97,99,107,0.3)] hover:text-[rgba(97,99,107,0.3)] transition-colors"
                     @click="activeIndex = activeIndex === numberOfItems - 1 ? 0 : activeIndex + 1">
                 <svg width="9" height="16" viewBox="0 0 9 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M1.24976 14.6458L8.01899 7.87658L1.24976 1.10735" stroke="currentColor"
