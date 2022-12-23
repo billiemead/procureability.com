5a535b9f58296ade384dd941b0e663a0abc5d485
brnteka <mpbrainwave@gmail.com>
2022-12-23 14:24:36 +0200

    [*] container width edits

--- wp-content/themes/jupiter-child/client-success-stories.php
+++ wp-content/themes/jupiter-child/client-success-stories.php
@@ -12,7 +12,7 @@
     <?php while (have_posts()) : the_post(); ?>
         <?php // get_template_part('partials/page', 'content'); ?>
         <section class="pt-[35px] md:pt-[60px] pb-[90px] md:pb-[135px]">
-            <div class="section-container">
+            <div class="max-w-[990px] px-[15px] box-border mx-auto">
                 <div class="mb-[16px] md:mb-[66px]">
                     <h1 class="text-[#404040] text-[30px] leading-[1.3] font-[600] uppercase"><?= get_the_title(); ?></h1>
                 </div>
