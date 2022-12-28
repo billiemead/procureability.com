d888598c596eccb171f9e1a2edb231a235165dc0
brnteka <mpbrainwave@gmail.com>
2022-12-28 22:12:43 +0200

    [*] hamburger inner elements edits

--- wp-content/themes/jupiter-child/partials/header/hamburger.php
+++ wp-content/themes/jupiter-child/partials/header/hamburger.php
@@ -1,7 +1,7 @@
 <button data-x-on:click="active = !active; mobileMenuActive = !mobileMenuActive"
         data-x-data="{active: false}" data-x-bind:class="active && 'active'"
         class="hamburger">
-    <div></div>
-    <div></div>
-    <div></div>
+    <span></span>
+    <span></span>
+    <span></span>
 </button>
