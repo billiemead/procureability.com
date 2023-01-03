651c59c47522081c34aebea901f2ca54149137e4
brnteka <mpbrainwave@gmail.com>
2023-01-03 14:22:57 +0200

    [*] footer image logo size

--- wp-content/themes/jupiter-child/partials/footer/logo.php
+++ wp-content/themes/jupiter-child/partials/footer/logo.php
@@ -1,3 +1,3 @@
-<a class="[&_img]:max-w-full [&_img]:h-auto [&_img]:block" href="<?= get_home_url(); ?>">
+<a class="[&_img]:max-w-[446px] [&_img]:h-auto [&_img]:block" href="<?= get_home_url(); ?>">
     <?= wp_get_attachment_image($args['footer_logo']['id'], 'full'); ?>
 </a>
