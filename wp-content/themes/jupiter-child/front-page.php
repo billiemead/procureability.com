f8194801a37fafd76e9561c96489d040cf0368fb
brnteka <mpbrainwave@gmail.com>
2023-01-06 17:57:39 +0200

    [*] front page files refactor

--- wp-content/themes/jupiter-child/front-page.php
+++ wp-content/themes/jupiter-child/front-page.php
@@ -1,39 +1,39 @@
 <?php get_header(); ?>
 
 <?php /** HERO */ ?>
-<?php get_template_part('partials/home-hero'); ?>
+<?php get_template_part('partials/front-page/home-hero'); ?>
 <?php /** !HERO */ ?>
 
 <?php /** WELCOME TO PROCUREABILITY */ ?>
-<?php get_template_part('partials/home-welcome-to-procureability'); ?>
+<?php get_template_part('partials/front-page/home-welcome-to-procureability'); ?>
 <?php /** !WELCOME TO PROCUREABILITY */ ?>
 
 <?php /** WHO WE ARE */ ?>
-<?php get_template_part('partials/home-who-we-are'); ?>
+<?php get_template_part('partials/front-page/home-who-we-are'); ?>
 <?php /** !WHO WE ARE */ ?>
 
 <?php /** OUR ADVANTAGES */ ?>
-<?php get_template_part('partials/home-our-advantages'); ?>
+<?php get_template_part('partials/front-page/home-our-advantages'); ?>
 <?php /** !OUR ADVANTAGES */ ?>
 
 <?php /** CLIENT SUCCESS STORIES */ ?>
-<?php get_template_part('partials/home-client-success-stories'); ?>
+<?php get_template_part('partials/front-page/home-client-success-stories'); ?>
 <?php /** !CLIENT SUCCESS STORIES */ ?>
 
 <?php /** PROCUREMENT REIMAGINED */ ?>
-<?php get_template_part('partials/home-procurement-reimagined'); ?>
+<?php get_template_part('partials/front-page/home-procurement-reimagined'); ?>
 <?php /** !PROCUREMENT REIMAGINED */ ?>
 
 <?php /** CTA */ ?>
-<?php get_template_part('partials/home-cta'); ?>
+<?php get_template_part('partials/front-page/home-cta'); ?>
 <?php /** !CTA */ ?>
 
 <?php /** RECENT THOUGHT LEADERSHIP */ ?>
-<?php get_template_part('partials/home-recent-thought-leadership'); ?>
+<?php get_template_part('partials/front-page/home-recent-thought-leadership'); ?>
 <?php /** !RECENT THOUGHT LEADERSHIP */ ?>
 
 <?php /** INDUSTRY LEADER IN PROCUREMENT */ ?>
-<?php get_template_part('partials/home-industry-leader-in-procurement'); ?>
+<?php get_template_part('partials/front-page/home-industry-leader-in-procurement'); ?>
 <?php /** !INDUSTRY LEADER IN PROCUREMENT */ ?>
 
 <?php get_footer(); ?>
