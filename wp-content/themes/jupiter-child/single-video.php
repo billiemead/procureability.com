43d3388a6fb2a8dfcac23794fb2e5782fdb991e8
brnteka <mpbrainwave@gmail.com>
2023-01-13 17:26:55 +0200

    [+] single video template

---
+++ wp-content/themes/jupiter-child/single-video.php
@@ -0,0 +1,27 @@
+<?php
+/*
+    Template Name: Single video
+    Template Post Type: page
+*/
+?>
+
+<?php get_header(); ?>
+
+<section id="video" class="py-[15px]">
+    <div class="max-w-[930px] px-[15px] mx-auto">
+        <?php if (have_posts()) : ?>
+            <?php while (have_posts()) : the_post(); ?>
+                <div class="bg-[#0095b0] px-[15px] pt-[15px] pb-[20px]">
+                    <div>
+                        <?php the_content(); ?>
+                    </div>
+                    <div class="text-center mt-[20px]">
+                        <div class="text-[19px] text-white font-[700]"><?= get_the_title(); ?></div>
+                    </div>
+                </div>
+            <?php endwhile; ?>
+        <?php endif; ?>
+    </div>
+</section>
+
+<?php get_footer(); ?>
