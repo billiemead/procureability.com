78a9cb9a1169b89c294067426da1138771cf022b
brnteka <mpbrainwave@gmail.com>
2023-01-13 14:51:32 +0200

    [*] managed services js edits

--- wp-content/themes/jupiter-child/assets/js/page/managed-services.js
+++ wp-content/themes/jupiter-child/assets/js/page/managed-services.js
@@ -1,31 +1,30 @@
-const issuesSwiper = new Swiper('#supportingYourSuccessSlider', {
-    slidesPerView: 1,
-    spaceBetween: 50,
-    loop: false,
-    pagination: {
-        el: '#supportingYourSuccessSliderPagination',
-    },
-    navigation: {
-        nextEl: '#supportingYourSuccessSliderNavNext',
-        prevEl: '#supportingYourSuccessSliderNavPrev',
-    },
-    autoHeight: true,
-    breakpoints: {
-        1240: {
-            autoHeight: false
-        },
-    }
-});
-
 export default () => ({
     modal: false,
+    slider: new Swiper('#supportingYourSuccessSlider', {
+        slidesPerView: 1,
+        spaceBetween: 50,
+        loop: false,
+        pagination: {
+            el: '#supportingYourSuccessSliderPagination',
+        },
+        navigation: {
+            nextEl: '#supportingYourSuccessSliderNavNext',
+            prevEl: '#supportingYourSuccessSliderNavPrev',
+        },
+        autoHeight: true,
+        breakpoints: {
+            1240: {
+                autoHeight: false
+            },
+        }
+    }),
     toggleModal() {
         this.modal = !this.modal;
     },
     setActiveSlide(index) {
         console.log('active ' + index);
 
-        issuesSwiper.slideTo(index)
+        this.slider.slideTo(index)
         this.toggleModal();
     }
 })
