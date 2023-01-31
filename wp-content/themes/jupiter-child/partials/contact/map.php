5d10062eef4a9fc8b552b84364686bae55d5efe0
brnteka <mpbrainwave@gmail.com>
2023-01-31 22:23:45 +0200

    [*] qa edits

--- wp-content/themes/jupiter-child/partials/contact/map.php
+++ wp-content/themes/jupiter-child/partials/contact/map.php
@@ -4,13 +4,13 @@
 <?php $map_socials = get_field('map_socials'); ?>
 
 <!--map-->
-<section class="py-[60px] relative">
+<section class="pb-[60px] md:py-[60px] relative">
     <div class="pt-[66.67%] !relative md:pt-0 md:!absolute md:inset-0">
         <iframe class="!absolute inset-0 md:!static md:inset-none" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7089.340846363897!2d-81.66357161314166!3d30.329325762853113!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x2f4304ee580a1ef7!2zMzDCsDE5JzQ1LjIiTiA4McKwMzknMzUuMiJX!5e0!3m2!1sen!2sua!4v1675194677408!5m2!1sen!2sua" width="100%" height="100%" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
     </div>
     <div class="max-w-[1200px] mx-auto relative pointer-events-none">
-        <div class="max-w-[300px] px-[15px] pointer-events-auto">
-            <div class="pt-[32px] px-[32px] pb-[43px] bg-white rounded-[6px] shadow-[0px_4px_24px_rgba(0,0,0,0.1)] before:content-[''] before:h-[4px] before:block before:absolute before:top-0 before:left-0 before:w-full before:bg-[#0098AE] before:opacity-40 relative overflow-hidden">
+        <div class="md:max-w-[300px] md:px-[15px] pointer-events-auto">
+            <div class="pt-[32px] px-[32px] pb-[43px] bg-white md:rounded-[6px] md:shadow-[0px_4px_24px_rgba(0,0,0,0.1)] before:content-[''] before:h-[4px] before:block before:absolute before:top-0 before:left-0 before:w-full before:bg-[#0098AE] before:opacity-40 relative overflow-hidden">
                 <?php if ($map_title) : ?>
                     <div class="mb-[16px]">
                         <div class="font-poppins text-[14px] leading-[1.5] text-[#0098AE] font-semibold tracking-[3px] uppercase">
