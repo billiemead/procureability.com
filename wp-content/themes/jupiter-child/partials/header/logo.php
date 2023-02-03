d55263b30d41ddd65c80ed37235d5d99d69c6cc0
brnteka <mpbrainwave@gmail.com>
2023-02-03 17:39:04 +0200

    [*] header logo edit

--- wp-content/themes/jupiter-child/partials/header/logo.php
+++ wp-content/themes/jupiter-child/partials/header/logo.php
@@ -2,10 +2,10 @@
     <div data-x-bind:class="{'xl:block': !sticky && isTransparent }" class="hidden">
         <?= wp_get_attachment_image(get_field('header_logo_light_tagline', 'option')['id'], 'full', false, array('class' => 'lazy-load-ignore block absolute top-[8px] w-[160px] h-auto')); ?>
     </div>
-    <div data-x-bind:class="{'xl:!block' : sticky && isTransparent || sticky && !isTransparent}" class="xl:hidden">
+    <div data-x-bind:class="{'xl:!block' : sticky && isTransparent}" class="xl:hidden">
         <?= wp_get_attachment_image(get_field('header_logo_dark', 'option')['id'], 'full', false, array('class' => 'lazy-load-ignore block absolute top-[8px] w-[160px] h-auto')); ?>
     </div>
-    <div data-x-bind:class="{'xl:!block' : !sticky && !isTransparent}" class="xl:hidden">
+    <div data-x-bind:class="{'xl:!block' : !sticky && !isTransparent || sticky && !isTransparent}" class="xl:hidden">
         <?= wp_get_attachment_image(get_field('header_logo_dark_tagline', 'option')['id'], 'full', false, array('class' => 'lazy-load-ignore block absolute top-[8px] w-[160px] h-auto')); ?>
     </div>
 </a>
