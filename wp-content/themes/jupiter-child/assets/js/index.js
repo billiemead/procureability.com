a45245044dacbceee8e456ed248760afd37ea72f
brnteka <mpbrainwave@gmail.com>
2023-02-27 22:53:38 +0200

    [*] front page ukraine modal body overflow

--- wp-content/themes/jupiter-child/assets/js/index.js
+++ wp-content/themes/jupiter-child/assets/js/index.js
@@ -1,8 +1,8 @@
 import $ from 'jquery';
 import 'magnific-popup';
 import 'owl.carousel';
-import { header, scrollToTop } from './header';
-import { disableBodyScroll, enableBodyScroll } from 'body-scroll-lock';
+import {header, scrollToTop} from './header';
+import {disableBodyScroll, enableBodyScroll, clearAllBodyScrollLocks} from 'body-scroll-lock';
 
 document.addEventListener('alpine:init', () => {
     Alpine.prefix("data-x-");
@@ -17,53 +17,53 @@ document.addEventListener('alpine:init', () => {
     });
 })
 
-if($.fancybox) {
-$('[data-fancybox="group"]')
-    .on('click', function (event) {
-        if (window.screen.width < 1024) {
-            event.stopImmediatePropagation();
-            window.location.href = $(this).data('href');
-        }
-    })
-    .fancybox({
-        hash: null,
-        toolbar: true,
-        infobar: false,
-        thumbs: {
-            autoStart: false
-        },
-        buttons: [
-            "share",
-            "thumbs",
-            "close"
-        ],
-        share: {
-            url: function (instance, item) {
-                return (
-                    (!instance.currentHash && !(item.type === "inline" || item.type === "html") ? item.origSrc || item.src : false) || window.location
-                );
+if ($.fancybox) {
+    $('[data-fancybox="group"]')
+        .on('click', function (event) {
+            if (window.screen.width < 1024) {
+                event.stopImmediatePropagation();
+                window.location.href = $(this).data('href');
+            }
+        })
+        .fancybox({
+            hash: null,
+            toolbar: true,
+            infobar: false,
+            thumbs: {
+                autoStart: false
             },
-            tpl:
-                '<div class="fancybox-share">' +
-                "<h1>{{SHARE}}</h1>" +
-                "<p>" +
-                '<a class="fancybox-share__button fancybox-share__button--fb" href="https://www.facebook.com/sharer/sharer.php?u={{url}}">' +
-                '<svg viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg"><path d="m287 456v-299c0-21 6-35 35-35h38v-63c-7-1-29-3-55-3-54 0-91 33-91 94v306m143-254h-205v72h196" /></svg>' +
-                "<span>Facebook</span>" +
-                "</a>" +
-                '<a class="fancybox-share__button fancybox-share__button--tw" href="https://twitter.com/intent/tweet?url={{url}}&text={{descr}}">' +
-                '<svg viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg"><path d="m456 133c-14 7-31 11-47 13 17-10 30-27 37-46-15 10-34 16-52 20-61-62-157-7-141 75-68-3-129-35-169-85-22 37-11 86 26 109-13 0-26-4-37-9 0 39 28 72 65 80-12 3-25 4-37 2 10 33 41 57 77 57-42 30-77 38-122 34 170 111 378-32 359-208 16-11 30-25 41-42z" /></svg>' +
-                "<span>Twitter</span>" +
-                "</a>" +
-                '<a class="fancybox-share__button fancybox-share__button--pt" href="https://www.pinterest.com/pin/create/button/?url={{url}}&description={{descr}}&media={{media}}">' +
-                '<svg viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg"><path d="m265 56c-109 0-164 78-164 144 0 39 15 74 47 87 5 2 10 0 12-5l4-19c2-6 1-8-3-13-9-11-15-25-15-45 0-58 43-110 113-110 62 0 96 38 96 88 0 67-30 122-73 122-24 0-42-19-36-44 6-29 20-60 20-81 0-19-10-35-31-35-25 0-44 26-44 60 0 21 7 36 7 36l-30 125c-8 37-1 83 0 87 0 3 4 4 5 2 2-3 32-39 42-75l16-64c8 16 31 29 56 29 74 0 124-67 124-157 0-69-58-132-146-132z" fill="#fff"/></svg>' +
-                "<span>Pinterest</span>" +
-                "</a>" +
-                "</p>" +
-                '<p><input class="fancybox-share__input" type="text" value="{{url_raw}}" /></p>' +
-                "</div>"
-        },
-    });
+            buttons: [
+                "share",
+                "thumbs",
+                "close"
+            ],
+            share: {
+                url: function (instance, item) {
+                    return (
+                        (!instance.currentHash && !(item.type === "inline" || item.type === "html") ? item.origSrc || item.src : false) || window.location
+                    );
+                },
+                tpl:
+                    '<div class="fancybox-share">' +
+                    "<h1>{{SHARE}}</h1>" +
+                    "<p>" +
+                    '<a class="fancybox-share__button fancybox-share__button--fb" href="https://www.facebook.com/sharer/sharer.php?u={{url}}">' +
+                    '<svg viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg"><path d="m287 456v-299c0-21 6-35 35-35h38v-63c-7-1-29-3-55-3-54 0-91 33-91 94v306m143-254h-205v72h196" /></svg>' +
+                    "<span>Facebook</span>" +
+                    "</a>" +
+                    '<a class="fancybox-share__button fancybox-share__button--tw" href="https://twitter.com/intent/tweet?url={{url}}&text={{descr}}">' +
+                    '<svg viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg"><path d="m456 133c-14 7-31 11-47 13 17-10 30-27 37-46-15 10-34 16-52 20-61-62-157-7-141 75-68-3-129-35-169-85-22 37-11 86 26 109-13 0-26-4-37-9 0 39 28 72 65 80-12 3-25 4-37 2 10 33 41 57 77 57-42 30-77 38-122 34 170 111 378-32 359-208 16-11 30-25 41-42z" /></svg>' +
+                    "<span>Twitter</span>" +
+                    "</a>" +
+                    '<a class="fancybox-share__button fancybox-share__button--pt" href="https://www.pinterest.com/pin/create/button/?url={{url}}&description={{descr}}&media={{media}}">' +
+                    '<svg viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg"><path d="m265 56c-109 0-164 78-164 144 0 39 15 74 47 87 5 2 10 0 12-5l4-19c2-6 1-8-3-13-9-11-15-25-15-45 0-58 43-110 113-110 62 0 96 38 96 88 0 67-30 122-73 122-24 0-42-19-36-44 6-29 20-60 20-81 0-19-10-35-31-35-25 0-44 26-44 60 0 21 7 36 7 36l-30 125c-8 37-1 83 0 87 0 3 4 4 5 2 2-3 32-39 42-75l16-64c8 16 31 29 56 29 74 0 124-67 124-157 0-69-58-132-146-132z" fill="#fff"/></svg>' +
+                    "<span>Pinterest</span>" +
+                    "</a>" +
+                    "</p>" +
+                    '<p><input class="fancybox-share__input" type="text" value="{{url_raw}}" /></p>' +
+                    "</div>"
+            },
+        });
 }
 
 $(function () {
@@ -111,10 +111,18 @@ $(function () {
 
     $('.ukraine-popup-mfp').magnificPopup({
         type: 'inline',
-        removalDelay: 500, //delay removal by X to allow out-animation
+        removalDelay: 500,
+        overflowY: 'scroll',
+        autoFocusLast: false,
         callbacks: {
-            beforeOpen: function() {
+            beforeOpen: function () {
                 this.st.mainClass = this.st.el.attr('data-effect');
+            },
+            close: function () {
+                clearAllBodyScrollLocks();
+            },
+            open: function () {
+                enableBodyScroll(this.content);
             }
         }
     });
@@ -129,13 +137,6 @@ $(function () {
         },
     });
 
-    $(document).on('scroll', function () {
-        if ($(window).outerWidth() > 1000 && $(window).scrollTop() > 0) {
-            $('header').addClass('a-sticky');
-        } else {
-            $('header').removeClass('a-sticky');
-        }
-    });
 
     $('.js-cp-testimonials').owlCarousel({
         items: 1,
