cc45b17705f6b0ddb1cfe1193a742ca28fee09dc
brnteka <mpbrainwave@gmail.com>
2023-03-10 19:23:48 +0200

    [*] page hero bg image position edits

--- wp-content/themes/jupiter-child/partials/page-hero.php
+++ wp-content/themes/jupiter-child/partials/page-hero.php
@@ -1,4 +1,4 @@
 <?php $hero_background_image = get_field('hero_background_image'); ?>
 <?php if ($hero_background_image) : ?>
-    <div class="hero-image rocket-lazyload" data-bg="<?= $hero_background_image['url']; ?>"></div>
+    <div class="hero-image rocket-lazyload xxl:bg-[center_top_22%]" data-bg="<?= $hero_background_image['url']; ?>"></div>
 <?php endif; ?>
