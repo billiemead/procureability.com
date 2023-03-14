143fe22d8854e7176337f20a7f993e4c01cf0050
brnteka <mpbrainwave@gmail.com>
2023-03-14 18:05:22 +0200

    [*] hero image edits team

--- wp-content/themes/jupiter-child/partials/page-hero-with-team-photo.php
+++ wp-content/themes/jupiter-child/partials/page-hero-with-team-photo.php
@@ -1,5 +1,7 @@
 <?php $hero_team_background_image = get_field('hero_team_background_image'); ?>
 <?php if ($hero_team_background_image) : ?>
-    <div class="h-[76px] md:h-[240px] rocket-lazyload bg-cover bg-[center_top_13%] bg-no-repeat"
-         data-bg="<?= $hero_team_background_image['url']; ?>"></div>
+    <div class="rocket-lazyload bg-cover bg-no-repeat"
+         data-bg="<?= $hero_team_background_image['url']; ?>">
+        <div class="pt-[16.87%]"></div>
+    </div>
 <?php endif; ?>
