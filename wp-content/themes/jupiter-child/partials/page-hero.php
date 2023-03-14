7ae648abef636ee2cbe5b04f9e35c5a34f690053
brnteka <mpbrainwave@gmail.com>
2023-03-14 11:41:22 +0200

    Revert "[*] hero image section refactor keep aspect ratio on every screen"
    This reverts commit 9691677fe8681168bc34b47fb87d8b428aeab223.

--- wp-content/themes/jupiter-child/partials/page-hero.php
+++ wp-content/themes/jupiter-child/partials/page-hero.php
@@ -2,12 +2,11 @@
 <?php $hero_remove_shadow = get_field('hero_remove_shadow'); ?>
 
 <?php if ($hero_background_image) : ?>
-    <div class="relative">
-        <div class="pb-[15.78%] bg-center bg-cover rocket-lazyload"
-             data-bg="<?= $hero_background_image['url']; ?>"></div>
+    <div class="h-[76px] bg-center bg-cover relative md:h-[240px] rocket-lazyload"
+         data-bg="<?= $hero_background_image['url']; ?>">
         <?php if (!$hero_remove_shadow) : ?>
             <div class="absolute inset-0 bg-[rgba(0,0,0,.3)]"></div>
-            <div class="absolute inset-0 bg-cover bg-center"
+            <div class="absolute inset-0"
                  style="background-image: url('<?= get_stylesheet_directory_uri(); ?>/assets/img/pages/supply-chain-and-logistics/bg-cover2.webp')"></div>
         <?php endif; ?>
     </div>
