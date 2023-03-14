9632709a9416ea7ce06a059937a732016cc539c1
brnteka <mpbrainwave@gmail.com>
2023-03-14 11:41:23 +0200

    Revert "[*] hero image optional shadow"
    This reverts commit 6f26348053a7165eb1911c8e96d1f2281a9be2f6.

--- wp-content/themes/jupiter-child/partials/page-hero.php
+++ wp-content/themes/jupiter-child/partials/page-hero.php
@@ -1,13 +1,5 @@
 <?php $hero_background_image = get_field('hero_background_image'); ?>
-<?php $hero_remove_shadow = get_field('hero_remove_shadow'); ?>
-
 <?php if ($hero_background_image) : ?>
     <div class="h-[76px] bg-center bg-cover relative md:h-[240px] rocket-lazyload"
-         data-bg="<?= $hero_background_image['url']; ?>">
-        <?php if (!$hero_remove_shadow) : ?>
-            <div class="absolute inset-0 bg-[rgba(0,0,0,.3)]"></div>
-            <div class="absolute inset-0"
-                 style="background-image: url('<?= get_stylesheet_directory_uri(); ?>/assets/img/pages/supply-chain-and-logistics/bg-cover2.webp')"></div>
-        <?php endif; ?>
-    </div>
+         data-bg="<?= $hero_background_image['url']; ?>"></div>
 <?php endif; ?>
