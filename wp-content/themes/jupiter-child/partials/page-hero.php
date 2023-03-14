19d92ccd1f2b7edc49772f908edd024b56a31cae
brnteka <mpbrainwave@gmail.com>
2023-03-14 11:41:24 +0200

    Revert "[*] hero image section refactor"
    This reverts commit 068c0b35b4a449b0d6adb179acc97901e08e1b29.

--- wp-content/themes/jupiter-child/partials/page-hero.php
+++ wp-content/themes/jupiter-child/partials/page-hero.php
@@ -1,5 +1,4 @@
 <?php $hero_background_image = get_field('hero_background_image'); ?>
 <?php if ($hero_background_image) : ?>
-    <div class="h-[76px] bg-center bg-cover relative md:h-[240px] rocket-lazyload"
-         data-bg="<?= $hero_background_image['url']; ?>"></div>
+    <div class="hero-image rocket-lazyload xxl:bg-[center_top_22%]" data-bg="<?= $hero_background_image['url']; ?>"></div>
 <?php endif; ?>
