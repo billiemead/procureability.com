cee723d663a18cec8843fc2f92b47a7cbfb9d61c
brnteka <mpbrainwave@gmail.com>
2023-03-23 20:54:32 +0200

    [*] add update images 6 pages edits

--- wp-content/themes/jupiter-child/partials/advisory/post-hero.php
+++ wp-content/themes/jupiter-child/partials/advisory/post-hero.php
@@ -3,7 +3,7 @@
 <?php $post_hero_background_image = get_field('post_hero_background_image'); ?>
 
 <section
-        class="pt-[48px] pb-[280px] lg:py-[138px] bg-[#F1F8F6] bg-no-repeat bg-[length:616px_auto] bg-[calc(50%+24px)_bottom] lg:bg-[length:1100px_auto] lg:bg-[calc(50%+550px-147px)_bottom]"
+        class="pt-[48px] pb-[280px] lg:py-[138px] bg-[#F1F8F6] bg-no-repeat bg-[length:616px_auto] bg-[calc(50%+24px)_bottom] lg:bg-[length:1100px_auto] lg:bg-[calc(50%+550px-50px)_bottom] xl:bg-[calc(50%+550px-147px)_bottom]"
         style="background-image: url('<?= $post_hero_background_image['url']; ?>')">
     <div class="max-w-[1200px] mx-auto px-[34px]">
         <div class="max-w-[516px] mx-auto lg:mx-0">
