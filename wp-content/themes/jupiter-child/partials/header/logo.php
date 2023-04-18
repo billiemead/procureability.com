632270fbd0edd8fa6dd149879c69c12568adf232
brnteka <mpbrainwave@gmail.com>
2023-04-18 19:15:02 +0300

    [*] header logo size

--- wp-content/themes/jupiter-child/partials/header/logo.php
+++ wp-content/themes/jupiter-child/partials/header/logo.php
@@ -1,11 +1,11 @@
 <a href="<?= get_home_url(); ?>">
     <div data-x-bind:class="{'xl:block': !sticky && isTransparent }" class="hidden">
-        <?= wp_get_attachment_image(get_field('header_logo_light_tagline', 'option')['id'], 'full', false, array('class' => 'lazy-load-ignore block absolute top-[8px] w-[160px] h-auto')); ?>
+        <?= wp_get_attachment_image(get_field('header_logo_light_tagline', 'option')['id'], 'full', false, array('class' => 'lazy-load-ignore block absolute top-[5px] w-[180px] h-auto')); ?>
     </div>
     <div data-x-bind:class="{'xl:!block' : sticky && isTransparent}" class="xl:hidden">
-        <?= wp_get_attachment_image(get_field('header_logo_dark', 'option')['id'], 'full', false, array('class' => 'lazy-load-ignore block absolute top-[8px] w-[160px] h-auto')); ?>
+        <?= wp_get_attachment_image(get_field('header_logo_dark', 'option')['id'], 'full', false, array('class' => 'lazy-load-ignore block absolute top-[5px] w-[180px] h-auto')); ?>
     </div>
     <div data-x-bind:class="{'xl:!block' : !sticky && !isTransparent || sticky && !isTransparent}" class="xl:hidden">
-        <?= wp_get_attachment_image(get_field('header_logo_dark_tagline', 'option')['id'], 'full', false, array('class' => 'lazy-load-ignore block absolute top-[8px] w-[160px] h-auto')); ?>
+        <?= wp_get_attachment_image(get_field('header_logo_dark_tagline', 'option')['id'], 'full', false, array('class' => 'lazy-load-ignore block absolute top-[5px] w-[180px] h-auto')); ?>
     </div>
 </a>
