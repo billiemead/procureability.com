16c0a2af2d14ca4fa278833045f9e2683ab3dde9
brnteka <mpbrainwave@gmail.com>
2023-08-30 14:47:16 +0300

    [*] privacy policy message footer

--- wp-content/themes/jupiter-child/footer.php
+++ wp-content/themes/jupiter-child/footer.php
@@ -5,6 +5,7 @@
 <?php $footer_button = get_field('footer_button', 'options'); ?>
 <?php $footer_bottom_text = get_field('footer_bottom_text', 'options'); ?>
 <?php $socials = get_field('socials', 'options'); ?>
+<?php $footer_privacy_policy_message = get_field('footer_privacy_policy_message', 'options'); ?>
 
 <footer class="bg-[#393A40]">
     <div class="pt-[33px] pb-[76px] ">
@@ -49,6 +50,15 @@
             </div>
         </div>
     </div>
+    <?php if ($footer_privacy_policy_message) : ?>
+        <div class="py-[16px] bg-[#61636B]">
+            <div class="text-center px-[15px]">
+                <div class="[&_p]:!font-poppins [&_p]:text-[14px] [&_p]:leading-normal [&_p]:text-white [&_p]:!m-0 [&_a]:text-white [&_a]:!underline [&_a]:!decoration-dotted [&_a]:underline-offset-[5px] [&_a]:hover:decoration-transparent [&_a]:hover:!text-white">
+                    <?= $footer_privacy_policy_message; ?>
+                </div>
+            </div>
+        </div>
+    <?php endif; ?>
     <div class="py-[20px] border-t border-t-[rgba(97,99,107,0.2)] border-solid">
         <div class="max-w-[1200px] px-[15px] mx-auto">
             <div class="lg:flex lg:items-center lg:justify-between">
