a34a3bdce2169337bfcc509e48c3e8a8159be6b9
brnteka <mpbrainwave@gmail.com>
2023-10-31 17:05:37 +0200

    [*] front page hero banner edits

--- wp-content/themes/jupiter-child/partials/front-page/home-hero.php
+++ wp-content/themes/jupiter-child/partials/front-page/home-hero.php
@@ -11,57 +11,46 @@
 <?php $frontpage_hero_ukraine_image = get_field('frontpage_hero_ukraine_image'); ?>
 <?php $frontpage_hero_ukraine_popup_text = get_field('frontpage_hero_ukraine_popup_text'); ?>
 
-    <section
-            class="bg-no-repeat bg-center bg-[auto_125%] xl:bg-cover xl:bg-[center_top_16%]"
-            data-no-lazy="1"
-            style="background-image: url('<?= $frontpage_hero_background_image['url'] ?>')">
-        <div class="pt-[104px] md:pt-[133px] xl:pt-[265px] pb-[60px] md:pb-[74px] xl:pb-[135px]">
-            <div class="px-[34px] xl:max-w-[1096px] mx-auto">
-                <h1 class="text-center font-poppins text-white text-[28px] md:text-[42px] leading-[1.2] md:leading-[1.27] font-[600] mb-[16px]">
-                    <?= $frontpage_hero_title; ?>
-                </h1>
-                <div class="md:max-w-[608px] xl:max-w-full mx-auto text-center font-poppins text-white text-[14px] md:text-[16px] xl:text-[24px] leading-[1.7] md:leading-[1.7] mb-[32px]"><?= $frontpage_hero_text; ?>
-                </div>
-                <div class="text-center">
-                    <a class="inline-block py-[12px] px-[48px] bg-[#3BA17C] rounded-[3px] relative group"
-                       href="<?= $frontpage_hero_button_link['url']; ?>">
-                        <div class="absolute inset-0 group-hover:opacity-0 opacity-100 transition-opacity duration-500 bg-no-repeat bg-center"
-                             style="background-image: url('<?= get_stylesheet_directory_uri(); ?>/assets/img/button-bg-pattern.svg')"
-                        ></div>
-                        <span class="font-poppins text-white text-[18px] leading-[1.5] font-[700] tracking-[0.1em] uppercase"><?= $frontpage_hero_button_text; ?></span></a>
-                </div>
+<?php $frontpage_hero_banner_title = get_field('frontpage_hero_banner_title'); ?>
+<?php $frontpage_hero_banner_text = get_field('frontpage_hero_banner_text'); ?>
+
+<section
+        class="bg-no-repeat bg-center bg-[auto_125%] xl:bg-cover xl:bg-[center_top_16%]"
+        data-no-lazy="1"
+        style="background-image: url('<?= $frontpage_hero_background_image['url'] ?>')">
+    <div class="pt-[104px] md:pt-[133px] xl:pt-[252px] pb-[60px] md:pb-[74px] xl:pb-[48px]">
+        <div class="px-[34px] xl:max-w-[1096px] mx-auto">
+            <h1 class="text-center font-poppins text-white text-[28px] md:text-[42px] leading-[1.2] md:leading-[1.27] font-[600] mb-[16px]">
+                <?= $frontpage_hero_title; ?>
+            </h1>
+            <div class="md:max-w-[608px] xl:max-w-full mx-auto text-center font-poppins text-white text-[14px] md:text-[16px] xl:text-[24px] leading-[1.7] md:leading-[1.7] mb-[32px]"><?= $frontpage_hero_text; ?>
             </div>
-        </div>
-        <div class="py-[12px] md:py-[10px] xl:py-[12px] bg-[#0098AE] bg-opacity-90">
-            <div class="px-[34px] xl:max-w-[1200px] mx-auto">
-                <div class="flex justify-center items-center">
-                    <div class="mr-[10px] md:mr-[12px] xl:mr-[16px] [&_img]:w-[39px] md:[&_img]:w-[60px] xl:[&_img]:w-[78px] [&_img]:h-auto">
-                        <?= wp_get_attachment_image($frontpage_hero_ukraine_image['id']); ?>
-                    </div>
-                    <div class="font-poppins text-white text-[12px] md:text-[16px] xl:text-[20px] leading-[1.46] md:leading-[1.4] tracking-[0.08em] md:tracking-[0.1em] font-[600] uppercase mr-[12px] md:mr-[15px] xl:mr-[20px]">
-                        <?= $frontpage_hero_ukraine_text_1; ?>
-                    </div>
-                    <span class="xl:max-w-[642px] leading-none">
-                    <span class="hidden xl:inline font-poppins text-white text-[14px] leading-[1.6]"><?= $frontpage_hero_ukraine_text_2; ?></span>
-                    <a data-effect="mfp-zoom-out" href="<?= $frontpage_hero_ukraine_read_more_link['url']; ?>"
-                       class="ukraine-popup-mfp font-poppins text-[#FFD700] text-[11px] xl:text-[14px] leading-[1.51] font-[500] transition-colors hover:text-white duration-500"><span><?= $frontpage_hero_ukraine_read_more_text; ?></span>
-                        <svg class="align-middle ml-[4px]" xmlns="http://www.w3.org/2000/svg" width="8" height="13"
-                             viewBox="0 0 8 13"
-                             fill="none">
-                            <path opacity="0.5" d="M1 11.5212L6.5 6.02124L1 1.02124" stroke="currentColor"
-                                  stroke-width="1.5"/>
-                        </svg>
-                    </a>
-                </span>
-                </div>
+            <div class="text-center">
+                <a class="inline-block py-[12px] px-[48px] bg-[#3BA17C] rounded-[3px] relative group"
+                   href="<?= $frontpage_hero_button_link['url']; ?>">
+                    <div class="absolute inset-0 group-hover:opacity-0 opacity-100 transition-opacity duration-500 bg-no-repeat bg-center"
+                         style="background-image: url('<?= get_stylesheet_directory_uri(); ?>/assets/img/button-bg-pattern.svg')"
+                    ></div>
+                    <span class="font-poppins text-white text-[18px] leading-[1.5] font-[700] tracking-[0.1em] uppercase"><?= $frontpage_hero_button_text; ?></span></a>
             </div>
-        </div>
-    </section>
-<?php if ($frontpage_hero_ukraine_popup_text) : ?>
-    <div class="ukraine-popup mfp-hide" id="ukraine-popup"
-         style="background-image: url(/wp-content/themes/jupiter-child/dist/img/pages/homepage/banner/popup-bg.jpg);">
-        <div class="ukraine-popup__text">
-            <?= $frontpage_hero_ukraine_popup_text ?>
         </div>
     </div>
-<?php endif; ?>
+    <?php if ($frontpage_hero_banner_title && $frontpage_hero_banner_text) : ?>
+        <div class="py-[12px] md:py-[10px] xl:py-[20px] bg-[#0098AE] bg-opacity-90">
+            <div class="px-[34px] xl:max-w-[1200px] mx-auto">
+                <div class="max-w-[858px] mx-auto">
+                    <div class="text-center mb-[4px]">
+                        <div class="font-poppins text-white text-[18px] leading-[1.5] font-semibold"><?= $frontpage_hero_banner_title; ?></div>
+                    </div>
+                </div>
+                <div class="max-w-[728px] mx-auto">
+                    <div class="text-center">
+                        <div class="[&_p]:!font-poppins [&_p]:text-white [&_p]:!text-[14px] [&_p]:!leading-[1.6] [&_p]:mb-0 [&_a]:text-[#FFD700] [&_a]:!font-poppins [&_a]:!text-[14px] [&_a]:!leading-[1.6] [&_a]:!underline [&_a:hover]:!no-underline [&_a:hover]:!text-[#FFD700] [&_a]:!decoration-dotted [&_a]:underline-offset-2 [&_a]:decoration-[#FFD700] [&_a:hover]:decoration-transparent">
+                            <?= $frontpage_hero_banner_text; ?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    <?php endif; ?>
+</section>
