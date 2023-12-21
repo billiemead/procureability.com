66750a5649ace714d18fe492ba11114467966e7a
Arti_1995 <tri5oddp@gmail.com>
2023-12-21 18:43:15 +0200

    [*] change banner on blog page

--- wp-content/themes/jupiter-child/partials/landing/banner.php
+++ wp-content/themes/jupiter-child/partials/landing/banner.php
@@ -6,15 +6,56 @@ $background = get_field('background_image_landing_banner');
 
 ?>
 
-<section class="pb-[40px] md:pb-[80px]">
-    <div class="box-border max-w-[984px] px-[80px] py-[80px] mx-auto bg-contain bg-no-repeat bg-right" style="background-image: url(<?= $background['url'] ?>); background-color: #F7F9FA;">
+<section class="pb-[40px] md:pb-[80px] px-[15px] landing-banner">
+    <div class="box-border
+                    max-w-[984px]
+                    px-[30px]
+                    py-[30px]
+                    sm:px-[80px]
+                    sm:py-[80px]
+                    mx-auto
+                    bg-contain
+                    bg-no-repeat
+                    bg-right
+                    landing-banner__text" style="background-image: url(<?= $background['url'] ?>); background-color: #F7F9FA;">
         <?php if ($title) : ?>
-            <h2 class="leading-8 font-poppins font-semibold text-sm max-w-[380px]" style="color: #393A3F; font-size: 1.6em;"><?= $title ?></h2>
+            <h2 class="leading-8b
+                        font-poppins
+                        font-semibold
+                        text-[1.4em]
+                        sm:text-[1.6em]
+                        sm:max-w-[100%]
+                        sm:max-w-[380px]" style="color: #393A3F;"><?= $title ?></h2>
         <?php endif; ?>
 
         <a
                 href="<?= $button['button_link_landing_banner'] ?>"
-                class="font-poppins text-[18px] leading-[1.27] text-transparent hover:text-transparent bg-clip-text brand-text-gradient inline-block mt-[1.2em] transition-all ease-in-out duration-300 relative after:content-[''] after:absolute after:w-full after:border-b-2 after:border-t-0 after:border-l-0 after:border-r-0 after:border-b-[#0098AE] after:opacity-100 after:block after:border-dotted hover:after:opacity-0 after:transition-all"
+                class="font-poppins
+                        sm:text-[18px]
+                        text-[14px]
+                        leading-[1.27]
+                        text-transparent
+                        hover:text-transparent
+                        bg-clip-text
+                        brand-text-gradient
+                        inline-block mt-[1.2em]
+                        transition-all
+                        ease-in-out
+                        duration-300
+                        relative
+                        after:content-['']
+                        after:absolute
+                        after:w-full
+                        after:border-b-2
+                        after:border-t-0
+                        after:border-l-0
+                        after:border-r-0
+                        after:border-b-[#0098AE]
+                        after:opacity-100
+                        after:block
+                        after:border-dotted
+                        hover:after:opacity-0
+                        after:transition-all"
                 title="<?= $button['button_text_landing_banner'] ?>"
                 style="color: #4DA880;"
         >
