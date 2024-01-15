3dc3f95bfc8c232609531047c8db62394eb6f3e2
Arti_1995 <tri5oddp@gmail.com>
2024-01-15 21:58:09 +0200

    [*] change webpack mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -56,7 +56,6 @@ mix
     .js(resources_path + 'assets/js/page/contact.js', resources_path + 'dist')
     .js(resources_path + 'assets/js/page/digital-services.js', resources_path + 'dist')
     .js(resources_path + 'assets/js/page/about.js', resources_path + 'dist')
-    .js(resources_path + 'assets/js/page/culture.js', resources_path + 'dist')
     .js(resources_path + 'assets/js/page/leadership.js', resources_path + 'dist')
     .js(resources_path + 'assets/js/page/thank-you.js', resources_path + 'dist')
 
