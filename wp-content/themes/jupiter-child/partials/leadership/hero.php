79f9f939a3484cc243eba6a34f2a29a680723c17
Arti_1995 <tri5oddp@gmail.com>
2024-01-15 17:13:35 +0200

    [*] change hero leadership page

--- wp-content/themes/jupiter-child/partials/leadership/hero.php
+++ wp-content/themes/jupiter-child/partials/leadership/hero.php
@@ -8,11 +8,11 @@
         style="background-image: url('<?= get_stylesheet_directory_uri(); ?>/assets/img/pages/leadership/triangle-blue.png')">
     <div class="max-w-[1200px] px-[15px] mx-auto">
         <div class="relative flex flex-col lg:flex-row">
-            <div class="w-[100%] lg:w-[56%] mb-[70px] lg:mb-[30px] lg:mb-0">
+            <div class="w-[100%] lg:w-[56%] mb-[30px] lg:mb-0">
                 <?php if ($hero_quote) : ?>
                     <div class="mb-[29px]">
-                        <div class="font-poppins text-[#393A40] text-[24px] md:text-[26px] leading-[1.55] font-medium">
-                        <span class="relative lg:!absolute lg:left-[-37px] lg:top-[-7px]">
+                        <div class="pl-[40px] font-poppins text-[#393A40] text-[24px] md:text-[26px] leading-[1.55] font-medium">
+                        <span class="relative lg:!absolute lg:left-0 lg:top-[-7px]">
                             <svg class="w-[30px] h-auto" viewBox="0 0 34 28" fill="none"
                                  xmlns="http://www.w3.org/2000/svg">
                                 <path d="M7.45333 27.1969C5.35708 27.1969 3.57139 26.4834 2.09625 25.0563C0.698749 23.55 0 21.7266 0 19.5861C0 18.3176 0.232917 17.0888 0.69875 15.8996C1.24222 14.7104 2.05743 13.2834 3.14437 11.6185L10.4812 0.796875L15.1396 3.88877L9.43312 12.8077C10.8306 13.0455 12.0728 13.7987 13.1598 15.0671C14.2467 16.2563 14.7902 17.7626 14.7902 19.5861C14.7902 21.7266 14.0526 23.55 12.5775 25.0563C11.18 26.4834 9.47194 27.1969 7.45333 27.1969ZM25.9138 27.1969C23.8175 27.1969 22.0318 26.4834 20.5567 25.0563C19.1592 23.55 18.4604 21.7266 18.4604 19.5861C18.4604 18.3176 18.6933 17.0888 19.1592 15.8996C19.7027 14.7104 20.5179 13.2834 21.6048 11.6185L28.9417 0.796875L33.6 3.88877L27.8936 12.8077C29.2911 13.0455 30.5333 13.7987 31.6202 15.0671C32.7072 16.2563 33.2506 17.7626 33.2506 19.5861C33.2506 21.7266 32.5131 23.55 31.0379 25.0563C29.6404 26.4834 27.9324 27.1969 25.9138 27.1969Z"
@@ -38,7 +38,7 @@
             </div>
 
             <div class="w-[100%] lg:w-[44%]">
-                <div id="bubbles">
+                <div id="bubbles" class="hidden md:block">
                     <div class="max-w-[528px] mx-auto relative flex">
                         <div class="bubble bg-center bg-no-repeat bg-cover inline-block relative rounded-full border-4 border-[#F7F9FA] border-solid basis-[154px] shrink-0 grow-0 h-[154px] z-10 top-0 left-0">
                             <div data-active="true" data-image="<?= $leadership_photos[0]; ?>"
