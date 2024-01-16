0d02ac8e4bd0e99392aeec610494d0c197769720
Arti_1995 <tri5oddp@gmail.com>
2024-01-16 13:19:10 +0200

    [*] change star bg image on Contact Page

--- wp-content/themes/jupiter-child/contact.php
+++ wp-content/themes/jupiter-child/contact.php
@@ -9,7 +9,7 @@
 <?php //https://support.advancedcustomfields.com/forums/topic/get_field-vs-get_fields-resource-calls/ ?>
 
 <?php get_header(); ?>
-    <div class="bg-[#F7F9FA] bg-[left_calc(50%+250px)_top_-196px] bg-no-repeat lg:bg-[41%] lg:bg-[left_calc(50%+560px)_top_67px]"
+    <div class="bg-[#F7F9FA] bg-[left_calc(50%+250px)_top_-196px] bg-no-repeat lg:bg-[length:41%] lg:bg-[left_calc(50%+560px)_top_67px]"
          style="background-image: url('<?= get_stylesheet_directory_uri(); ?>/assets/img/brand-star.svg')">
         <?php get_template_part('partials/contact/hero'); ?>
 
