26087eea216aa9a848b1e3868244b788c1c7bb1c
Arti_1995 <tri5oddp@gmail.com>
2024-01-18 15:17:43 +0200

    [*] change width of the video block on blog page

--- wp-content/themes/jupiter-child/partials/blog/content.php
+++ wp-content/themes/jupiter-child/partials/blog/content.php
@@ -7,7 +7,7 @@ $rightColumnContent = get_field('right_column');
 <?php if (have_posts()) : ?>
     <?php while (have_posts()) : the_post(); ?>
         <div class="max-w-[960px] px-[15px] mx-auto">
-            <div class="grid grid-cols-1 sm:grid-cols-[auto_312px] gap-[30px] mb-[50px]">
+            <div class="grid grid-cols-1 sm:grid-cols-[auto_400px] gap-[30px] mb-[50px]">
                 <div>
                     <h1 class="section-h1"><?= get_the_title(); ?></h1>
                     <div class="cms-content [&_p]:!text-[18px]">
