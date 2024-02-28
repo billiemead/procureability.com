5d6649bda329b5613c84bdc7ce555d83881e8992
Arti_1995 <tri5oddp@gmail.com>
2024-02-28 17:38:56 +0200

    [*] change text/title colors and background image in the Supply chain and logistics page template

--- wp-content/themes/jupiter-child/partials/page-content.php
+++ wp-content/themes/jupiter-child/partials/page-content.php
@@ -1,7 +1,17 @@
-<section class="section section--content">
-    <div class="section-container">
-        <h1 class="section-h1"><?= get_the_title(); ?></h1>
-        <div class="cms-content">
+<section class="relative box-border pb-0 pt-[25px] md:pt-[50px]">
+    <div class="max-w-[930px] px-[15px] mx-auto">
+        <h1 class="text-[#005E6B] font-poppins text-[16px] font-medium leading-[1.4] mb-[16px]"><?= get_the_title(); ?></h1>
+        <div class="overflow-hidden
+                [&_h2]:font-poppins
+                [&_h2]:text-[#393A40]
+                [&_h2]:text-[24px]
+                [&_h2]:font-medium
+                [&_h2]:leading-[1.33333]
+                [&_h2]:mb-[16px]
+                [&_p]:!font-poppins
+                [&_p]:text-[#393A40]
+                [&_p]:mb-[16px]"
+        >
             <?php the_content(); ?>
         </div>
     </div>
