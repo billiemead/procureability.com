1199674d18dcc8faea62f3e9b7e599abc21b24eb
Arti_1995 <tri5oddp@gmail.com>
2024-03-01 16:58:16 +0200

    [*] change color link and font family in the Simple page layout with leadership page template

--- wp-content/themes/jupiter-child/partials/page-content.php
+++ wp-content/themes/jupiter-child/partials/page-content.php
@@ -2,17 +2,17 @@
     <div class="max-w-[930px] px-[15px] mx-auto">
         <h1 class="text-[#005E6B] font-poppins text-[16px] font-medium leading-[1.4] mb-[16px]"><?= get_the_title(); ?></h1>
         <div class="overflow-hidden
-                [&_h2]:font-poppins
-                [&_h2]:text-[#393A40]
-                [&_h2]:text-[24px]
-                [&_h2]:font-medium
-                [&_h2]:leading-[1.33333]
-                [&_h2]:mb-[16px]
-                [&_p]:!font-poppins
-                [&_p]:text-[#393A40]
-                [&_p]:mb-[16px]
-                [&_a]:text-[#005E6B]
-                [&_a]:font-poppins"
+                    [&_h2]:font-poppins
+                    [&_h2]:text-[#393A40]
+                    [&_h2]:text-[24px]
+                    [&_h2]:font-medium
+                    [&_h2]:leading-[1.33333]
+                    [&_h2]:mb-[16px]
+                    [&_p]:!font-poppins
+                    [&_p]:text-[#393A40]
+                    [&_p]:mb-[16px]
+                    [&_a]:text-[#005E6B]
+                    [&_a]:font-poppins"
         >
             <?php the_content(); ?>
         </div>
