edb2fdd64558603ac02c4f79aae9df535586a633
Arti_1995 <tri5oddp@gmail.com>
2024-03-08 13:30:00 +0200

    [*] set pum cookies (popup maker)

--- wp-content/themes/jupiter-child/assets/js/index.js
+++ wp-content/themes/jupiter-child/assets/js/index.js
@@ -128,8 +128,9 @@ $(document).ready(event => {
         $(document).trigger('resize');
     });
 
-    $('body').on('submit', "form#hsForm_794fc76d-49dd-4cbb-b777-bb844d175ca0, form#hsForm_8ddc5061-7eb7-4dca-8d14-7b35f3d86482", eventData => {
+    $('body').on('submit', "form#hsForm_e5ae7576-0dc2-4762-8b72-373c739ff93a, form#hsForm_794fc76d-49dd-4cbb-b777-bb844d175ca0, form#hsForm_8ddc5061-7eb7-4dca-8d14-7b35f3d86482", eventData => {
         $(document).trigger('resize');
+        $('#pum-31144').trigger('pumSetCookie');
     });
 });
 
