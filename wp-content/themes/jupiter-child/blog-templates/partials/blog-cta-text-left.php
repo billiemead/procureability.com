99b4c9024022afd8f79dde77e240e663c6aae8c0
Arti_1995 <tri5oddp@gmail.com>
2024-04-01 12:58:29 +0300

    [-] remove link from the development

--- wp-content/themes/jupiter-child/blog-templates/partials/blog-cta-text-left.php
+++ wp-content/themes/jupiter-child/blog-templates/partials/blog-cta-text-left.php
@@ -7,7 +7,7 @@
             </div>
             <div>
                 <a href="<?= $args['button_link'] ?>" target="_blank" class="inline-block py-[12px] px-[30px] bg-[#3BA17C] rounded-[3px] group relative" title="<?= $args['button_text'] ?>">
-                    <div class="absolute inset-0 group-hover:opacity-0 opacity-100 transition-opacity duration-500 bg-no-repeat bg-center" style="background-image: url('https://development.procureability.com/wp-content/uploads/2024/01/button-bg-pattern.svg')"></div>
+                    <div class="absolute inset-0 group-hover:opacity-0 opacity-100 transition-opacity duration-500 bg-no-repeat bg-center" style="background-image: url('https://procureability.com/wp-content/uploads/2024/01/button-bg-pattern.svg')"></div>
                     <span class="font-poppins text-white text-[18px] leading-[1.5] font-[700] tracking-[0.1em] uppercase"><?= $args['button_text'] ?></span>
                 </a>
             </div>
