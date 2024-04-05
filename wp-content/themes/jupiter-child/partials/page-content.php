a74222d6ba3c67f5e8778e0ee04d36fa4482565e
Arti_1995 <tri5oddp@gmail.com>
2024-04-05 19:00:20 +0300

    [*] change image size in the page content template part

--- wp-content/themes/jupiter-child/partials/page-content.php
+++ wp-content/themes/jupiter-child/partials/page-content.php
@@ -16,7 +16,9 @@
                     [&_li]:text-[#393A40]
                     [&_li]:font-poppins
                     font-poppins
-                    text-[#393A40]"
+                    text-[#393A40]
+                    [&_img]:h-auto
+                    [&_img]:max-w-[100%]"
         >
             <?php the_content(); ?>
         </div>
