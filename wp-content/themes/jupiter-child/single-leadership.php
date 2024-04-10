a3b2e9aef9438b9b0cca5ba792b70ca70a1f5d2d
Arti_1995 <tri5oddp@gmail.com>
2024-04-10 13:46:57 +0300

    [*] change mobile view of bio popup and add social links in leadership single page

--- wp-content/themes/jupiter-child/single-leadership.php
+++ wp-content/themes/jupiter-child/single-leadership.php
@@ -8,6 +8,7 @@
                     $leadership_title = get_field('leadership_title');
                     $leadership_bio = get_field('leadership_bio');
                     $leadership_casual_image = get_field('leadership_casual_image');
+                    $leadership_socials = get_field('leadership_socials');
                     ?>
 
                     <img class="block lg:inline-block mx-auto lg:float-right lg:ml-[2em] lg:mr-0 mb-[2em] w-full max-w-[378px] lg:max-w-[calc(100%*(1-1/1.618))]"
@@ -16,6 +17,21 @@
                     <h1 class="text-[#f97352] text-[16px] leading-[1.66] font-bold mb-0"><?= $leadership_name; ?></h1>
                     <span class="inline-block text-[#f97352] text-[16px] leading-[1.66] font-bold mb-[20px]"><?= $leadership_title; ?></span>
                     <?= apply_filters('the_content', $leadership_bio); ?>
+
+                    <?php if ($leadership_socials) : ?>
+                        <div class="mt-[32px] space-x-[30px]">
+                            <?php foreach ($leadership_socials as $leadership_social) : ?>
+                                <a href="<?= $leadership_social['url']; ?>"
+                                   target="_blank"
+                                   class="inline-flex items-center group">
+                                    <img class="mr-[12px]"
+                                         src="<?= get_stylesheet_directory_uri() . '/assets/img/socials/' . $leadership_social['network']['value'] . '.svg'; ?>"
+                                         alt="<?= $leadership_social['network']['label']; ?>">
+                                    <span class="font-poppins text-[16px] leading-[1.6] text-[#5A5A5A] underline decoration-transparent group-hover:decoration-current transition-colors"><?= $leadership_social['network']['label']; ?></span>
+                                </a>
+                            <?php endforeach; ?>
+                        </div>
+                    <?php endif; ?>
                 <?php endwhile; ?>
             <?php endif; ?>
         </div>
