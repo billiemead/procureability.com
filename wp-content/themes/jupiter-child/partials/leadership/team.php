f6107c5bd3bbd434be6f649b299443fb4f8b4c01
Arti_1995 <tri5oddp@gmail.com>
2024-04-10 18:26:06 +0300

    [*] fix sticky social links in bio popup

--- wp-content/themes/jupiter-child/partials/leadership/team.php
+++ wp-content/themes/jupiter-child/partials/leadership/team.php
@@ -138,7 +138,7 @@
                                                         <div class="font-poppins text-[#61636B] text-[18px] leading-normal"><?= $leadership_title; ?></div>
                                                     </div>
                                                 </div>
-                                                <div class="flex flex-wrap-reverse gap-[20px] flex-0 sm:!grid sm:gap-[60px] grid-cols-[53.09%_auto] overflow-y-auto overflow-x-hidden px-[40px] lg:px-[80px]" style="transform: translateZ(0);">
+                                                <div class="gap-[20px] flex-0 sm:!grid sm:gap-[60px] grid-cols-[53.09%_auto] overflow-y-auto overflow-x-hidden px-[40px] lg:px-[80px]" style="transform: translateZ(0);">
                                                     <div>
                                                         <?php if ($leadership_socials) : ?>
                                                             <div class="space-x-[30px] sticky top-[86.5%] pt-[26.9px] bg-[#fff]">
