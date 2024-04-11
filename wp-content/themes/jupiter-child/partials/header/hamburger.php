7d127639b5cfb17b48dc042fa22bcf717b5ef695
Arti_1995 <tri5oddp@gmail.com>
2024-04-11 11:47:50 +0300

    [*] change testimonial colors and color of read more button

--- wp-content/themes/jupiter-child/partials/header/hamburger.php
+++ wp-content/themes/jupiter-child/partials/header/hamburger.php
@@ -1,6 +1,8 @@
 <button data-x-on:click="active = !active; mobileMenuActive = !mobileMenuActive"
         data-x-data="{active: false}" data-x-bind:class="active && 'active'"
-        class="hamburger">
+        class="hamburger"
+        aria-label="Menu"
+>
     <span></span>
     <span></span>
     <span></span>
