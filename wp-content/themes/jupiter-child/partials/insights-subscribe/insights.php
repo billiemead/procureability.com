22d9564fc219880d4601ae80e83e8a3e80f4a012
Arti_1995 <tri5oddp@gmail.com>
2024-04-12 14:21:15 +0300

    [*] change subscribe button bg color

--- wp-content/themes/jupiter-child/partials/insights-subscribe/insights.php
+++ wp-content/themes/jupiter-child/partials/insights-subscribe/insights.php
@@ -46,7 +46,7 @@
         <?php endif; ?>
         <?php if ($insights_button && $form_form) : ?>
             <div class="text-center mt-[56px]">
-                <button class="inline-block bg-[#155B39] pt-[13px] pb-[11px] lg:p-[16px] max-w-[300px] lg:max-w-[420px] w-full uppercase  rounded-[3px] group relative box-border border-none"
+                <button class="inline-block bg-[#3ba17c] pt-[13px] pb-[11px] lg:p-[16px] max-w-[300px] lg:max-w-[420px] w-full uppercase  rounded-[3px] group relative box-border border-none"
                         data-x-data="{}"
                         data-x-on:click="document.getElementById('subscribe').scrollIntoView({ behavior: 'smooth' })"
                 >
